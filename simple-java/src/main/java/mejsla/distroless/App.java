a313693a65b61e1252b91f7aa5a1890acf294167
Gustaf Cele <gustaf.cele@mejsla.se>
2024-09-24 19:11:43 +0200

    Add simple java example

---
+++ simple-java/src/main/java/mejsla/distroless/App.java
@@ -0,0 +1,10 @@
+package mejsla.distroless;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, distroless Java world!");
+    }
+}
